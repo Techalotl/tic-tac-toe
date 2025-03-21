d5228aa7d76d9e1c0bbb9b1b59748e8af53ff898
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-03-20 18:46:58 -0600

    Add first two functions to play

---
+++ script.js
@@ -0,0 +1,69 @@
+function gameBoard() {
+  const squaresPerSide = 3;
+  const board = [];
+
+  for (let i = 0; i < squaresPerSide; i++) {
+      board[i] = [];
+      for (let j = 0; j < squaresPerSide; j++) {
+          board[i].push("-");
+      }
+  }
+  console.log(board);
+
+  const getBoard = () => board;
+
+  const putMark = (row, column, playerMark) => {
+    if (board[row][column] === "-") {
+      board[row][column] = playerMark;
+      // console.log(board);
+    } else {
+      console.log("That square is occupied, dummy!");
+      return;
+    }
+  };
+
+  const printBoard = () => console.log(board);
+
+  return { getBoard, putMark, printBoard }
+}
+
+function users() {
+const playerOneName = prompt("Player one: enter your name");
+const playerTwoName = prompt("Player two: enter your name");
+const player = [
+  {
+    name: playerOneName,
+    mark: "x",
+  },
+  {
+    name: playerTwoName,
+    mark: "o",
+  }
+]
+let activePlayer = player[0];
+const getActivePlayer = () => activePlayer;
+const switchPlayerTurn = () => {
+  activePlayer === player[0] ? activePlayer = player[1] : activePlayer = player[0];
+}
+return { getActivePlayer, switchPlayerTurn }
+}
+
+function playing() {
+const players = users();
+const board = gameBoard();
+const newRound = () => {
+  board.getBoard();
+  console.log(`It's ${players.getActivePlayer().name} turn`);
+}
+const playRound = (row, column) => {
+  board.putMark(row, column, players.getActivePlayer().mark);
+  console.log(`${players.getActivePlayer().name} has put ${players.getActivePlayer().mark} into row ${row}, column ${column}`);
+  players.switchPlayerTurn();
+  newRound();
+}
+
+newRound();
+return { playRound };
+}
+
+const game = playing();
