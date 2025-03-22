0e75bd0d8457ae284215e5ecb122282afff0ce40
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-03-21 18:04:39 -0600

    Fix indentation

--- script.js
+++ script.js
@@ -21,48 +21,48 @@ function gameBoard() {
 }
 
 function users() {
-const playerOneName = prompt("Player one: enter your name");
-const playerTwoName = prompt("Player two: enter your name");
-const player = [
-  {
-    name: playerOneName,
-    mark: "x",
-  },
-  {
-    name: playerTwoName,
-    mark: "o",
+  const playerOneName = prompt("Player one: enter your name");
+  const playerTwoName = prompt("Player two: enter your name");
+  const player = [
+    {
+      name: playerOneName,
+      mark: "x",
+    },
+    {
+      name: playerTwoName,
+      mark: "o",
+    }
+  ]
+  let activePlayer = player[0];
+  const getActivePlayer = () => activePlayer;
+  const switchPlayerTurn = () => {
+    activePlayer === player[0] ? activePlayer = player[1] : activePlayer = player[0];
   }
-]
-let activePlayer = player[0];
-const getActivePlayer = () => activePlayer;
-const switchPlayerTurn = () => {
-  activePlayer === player[0] ? activePlayer = player[1] : activePlayer = player[0];
-}
-return { getActivePlayer, switchPlayerTurn }
+  return { getActivePlayer, switchPlayerTurn }
 }
 
 function playing() {
-const players = users();
-const board = gameBoard();
-const newRound = () => {
-  board.printBoard();
-  console.log(`It's ${players.getActivePlayer().name} turn`);
-}
-const playRound = (row, column) => {
-  if (board.getBoard()[row][column] === "-") {
-    board.putMark(row, column, players.getActivePlayer().mark);
-    console.log(`${players.getActivePlayer().name} has put "${players.getActivePlayer().mark}" into row ${row}, column ${column}`);
-    players.switchPlayerTurn();
-    newRound();
-  } else {
-      console.log("That square is occupied, dummy!")
-      newRound();
-      return;
+  const players = users();
+  const board = gameBoard();
+  const newRound = () => {
+    board.printBoard();
+    console.log(`It's ${players.getActivePlayer().name} turn`);
   }
-}
+  const playRound = (row, column) => {
+    if (board.getBoard()[row][column] === "-") {
+      board.putMark(row, column, players.getActivePlayer().mark);
+      console.log(`${players.getActivePlayer().name} has put "${players.getActivePlayer().mark}" into row ${row}, column ${column}`);
+      players.switchPlayerTurn();
+      newRound();
+    } else {
+        console.log("That square is occupied, dummy!")
+        newRound();
+        return;
+    }
+  }
 
-newRound();
-return { playRound };
+  newRound();
+  return { playRound };
 }
 
 const game = playing();
